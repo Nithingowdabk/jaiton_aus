5efdbc9dc40a03a8767e6f3307305ed5384b8d2b
Nithin Gowda B K <nithinkempegowda2003@gmail.com>
2026-07-31 13:42:05 +0530

    Removed Global Delivery Network section from website layout per user request (index.php & components/global-presence.php)

--- index.php
+++ index.php
@@ -18,7 +18,6 @@ include_once 'components/why-jaiton.php';
 include_once 'components/mission-vision.php';
 include_once 'components/industries.php';
 include_once 'components/featured.php';
-include_once 'components/global-presence.php';
 include_once 'components/cta.php';
 include_once 'components/contact-preview.php';
 
